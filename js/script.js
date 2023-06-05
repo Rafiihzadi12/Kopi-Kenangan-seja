aa7fee66e21413060f4af0db71677cb093d72726
rafiihzadi <rafiihzadi@gmail.com>
2023-06-05 09:37:57 +0700

    update

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Toggel class active
+const navbarNav = document.querySelector(".navbar-nav");
+//ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
